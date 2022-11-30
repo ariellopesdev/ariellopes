e7d9505d7dd47893828801adad0d2842e84c5d40
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-11-30 02:46:37 -0300

    Updated Especialidades.php and images

--- pages/especialidades.php
+++ pages/especialidades.php
@@ -2,55 +2,64 @@
 <section class="especialidades">
     <div class="center">
         <h1>Especialidades</h1>
+        <div class="wrap">
         <div class="w33 left box-especialidades">
-            <h3><i class="fa-brands fa-css3"></i></h3>
+            <img src="<?php echo INCLUDE_PATH; ?>imagens/css.png">
             <h4>CSS3</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><i class="fa-brands fa-html5"></i></h3>
+            <img src="<?php echo INCLUDE_PATH; ?>imagens/html.png">
             <h4>HTML5</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><i class="fa-brands fa-bootstrap"></i></h3>
+            <img id="bootstrap" src="<?php echo INCLUDE_PATH; ?>imagens/bootstrap.png">
             <h4>Bootstrap</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><i class="fa-brands fa-square-js"></i></h3>
+        <img src="<?php echo INCLUDE_PATH; ?>imagens/javascript.png">
             <h4>Javascript</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><img src="<?php echo INCLUDE_PATH; ?>imagens/c.png"> </h3>
+            <img src="<?php echo INCLUDE_PATH; ?>imagens/c.png">
             <h4>C</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><img src="<?php echo INCLUDE_PATH; ?>imagens/c#.jpeg"> </h3>
+            <img src="<?php echo INCLUDE_PATH; ?>imagens/csharp.png">
             <h4>C#</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
         <div class="w33 left box-especialidades">
-            <h3><i class="fa-brands fa-php"></i></h3>
+            <img id="php" src="<?php echo INCLUDE_PATH; ?>imagens/php.png">
             <h4>PHP</h4>
             <label for="range">Nível de conhecimento (entre 1 e 5):</label>
             <input type="range" id="nivel" name="volume" min="1" max="5">
         </div>
         <!--w33 left box-especialidades-->
+        <div class="w33 left box-especialidades">
+            <img id="ingles" src="<?php echo INCLUDE_PATH; ?>imagens/ingles.png">
+            <h4>Inglês</h4>
+            <label for="range">Nível de conhecimento (entre 1 e 5):</label>
+            <input type="range" id="nivel" name="volume" min="1" max="5">
+        </div>
+        <!--w33 left box-especialidades-->
+        </div><!--wrap-->
         <div class="clear"></div>
     </div>
     <!--center-->
