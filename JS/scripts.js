d8f55bea298182e90132fcce11414ff05c13adc2
ariellopesdev <lubyel.lopes@gmail.com>
2023-01-22 19:15:34 -0300

    Update site

--- JS/scripts.js
+++ JS/scripts.js
@@ -33,4 +33,12 @@ $(function () {
             listaMenu.slideToggle();
         }
     })
+    $('.nav a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+                targetOffset = $(id).offset().top;
+        $('html, body').animate({ 
+            scrollTop: targetOffset - 100
+        }, 500);
+    });
 })
